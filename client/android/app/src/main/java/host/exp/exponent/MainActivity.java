99336ad6311f90b11c6981a5adb5239ca7acff35
Evan Bacon <baconbrix@gmail.com>
2018-10-05 20:15:28 -0700

    Added iOS support

--- client/android/app/src/main/java/host/exp/exponent/MainActivity.java
+++ client/android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -3,6 +3,21 @@ package host.exp.exponent;
 import android.os.Bundle;
 
 import com.facebook.react.ReactPackage;
+import expo.modules.firebase.analytics.FirebaseAnalyticsPackage;
+import expo.modules.firebase.app.FirebaseAppPackage;
+import expo.modules.firebase.auth.FirebaseAuthPackage;
+// import expo.modules.firebase.fabric.crashlytics.FirebaseCrashlyticsPackage;
+import expo.modules.firebase.database.FirebaseDatabasePackage;
+import expo.modules.firebase.firestore.FirebaseFirestorePackage;
+// import expo.modules.firebase.functions.FirebaseFunctionsPackage;
+import expo.modules.firebase.instanceid.FirebaseInstanceIDPackage;
+// import expo.modules.firebase.invites.FirebaseInvitesPackage;
+// import expo.modules.firebase.links.FirebaseLinksPackage;
+import expo.modules.firebase.messaging.FirebaseMessagingPackage;
+import expo.modules.firebase.notifications.FirebaseNotificationsPackage;
+import expo.modules.firebase.performance.FirebasePerformancePackage;
+// import expo.modules.firebase.remoteconfig.FirebaseRemoteConfigPackage;
+import expo.modules.firebase.storage.FirebaseStoragePackage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,7 +53,17 @@ public class MainActivity extends DetachActivity {
   @Override
   public List<Package> expoPackages() {
     // Here you can add your own packages.
-    return super.expoPackages();
+    return Arrays.<Package>asList(new FirebaseAppPackage(), new FirebaseAnalyticsPackage(), new FirebaseAuthPackage(),
+        // new FirebaseCrashlyticsPackage(),
+        new FirebaseDatabasePackage(), new FirebaseFirestorePackage(),
+        // new FirebaseFunctionsPackage(),
+        new FirebaseInstanceIDPackage(),
+        // new FirebaseInvitesPackage(),
+        // new FirebaseLinksPackage(),
+        new FirebaseMessagingPackage(), new FirebaseNotificationsPackage(), new FirebasePerformancePackage(),
+        // new FirebaseRemoteConfigPackage(),
+        new FirebaseStoragePackage());
+    // return super.expoPackages();
   }
 
   @Override
