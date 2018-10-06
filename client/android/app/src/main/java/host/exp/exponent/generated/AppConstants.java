60f1b0c28888c1f51704ec7d3d67d11cd079ab99
Evan Bacon <baconbrix@gmail.com>
2018-10-06 01:38:39 -0700

    deployed ios build 😍

--- client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ client/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -25,9 +25,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@bacon/bute/index.exp", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40bacon%2Fbute%2F0.0.0%2F140717e0e440cb8299afeb4f78532740-30.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
--- client/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ client/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expf323ea36d115447fa3827c6dcb512227://10.0.0.13:19000";
 
 }
