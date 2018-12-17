ad7fc73732d1b3c6fe655b8e705a71ffaadcecc3
Evan Bacon <baconbrix@gmail.com>
2018-12-17 14:13:05 -0800

    fixed chat background image issue
    * removed service workers
    * removed Expo notification register
    * removed old web entry

--- client/src/components/chat/ChatBackground.js
+++ client/src/components/chat/ChatBackground.js
@@ -3,15 +3,12 @@
 // @author Evan Bacon / https://github.com/EvanBacon
 //
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 import MetaData from '../../constants/Meta';
-import Circle from '../primitives/Circle';
-import LoadingImage from '../Image/ProgressImage';
+import ProfileImage from '../Image/ProfileImage';
 import Meta from '../Meta';
-import styles from '../styles';
-import AvatarImage from '../Image/AvatarImage';
 
 export default class EmptyChat extends React.PureComponent {
   render() {
@@ -47,27 +44,13 @@ export default class EmptyChat extends React.PureComponent {
             easing="ease-out-back"
             animation="zoomIn"
           >
-            <Circle
-              style={StyleSheet.flatten([
-                { width: '60%', backgroundColor: 'white' },
-                styles.border,
-                styles.shadow,
-              ])}
-            >
-              <AvatarImage
-                name={name}
-                avatar={image}
-                textStyle={{ fontSize: 64 }}
-                avatarStyle={{
-                  aspectRatio: 1,
-                  resizeMode: 'cover',
-                  flex: 1,
-                  width: '100%',
-                  height: '100%',
-                  borderRadius: 0,
-                }}
-              />
-            </Circle>
+            <ProfileImage
+              name={name}
+              image={image}
+              size={Dimensions.get('window').width * 0.6}
+              lightbox={true}
+              isUser={false}
+            />
           </Animatable.View>
         )}
         <Animatable.View
