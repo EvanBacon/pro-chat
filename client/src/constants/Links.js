b38f347b21f200543b3edb4c6cdc36c58506ad19
Evan Bacon <baconbrix@gmail.com>
2018-12-18 11:15:29 -0800

    Update Links.js

--- client/src/constants/Links.js
+++ client/src/constants/Links.js
@@ -1,13 +1,26 @@
 const baseUrl = 'https://github.com/evanbacon/expo-chat/';
 export default {
-  privacy: `${baseUrl}blob/master/PRIVACY.md`,
-  jobs: `${baseUrl}blob/master/JOBS.md`,
+  privacy: `http://rialastudio.com/bute/privacy/`,
+  // jobs: `${baseUrl}blob/master/JOBS.md`,
   support: `${baseUrl}issues`,
-  terms: `${baseUrl}blob/master/TERMS.md`,
-  licenses: `${baseUrl}blob/master/LICENSE.md`,
-  eula: `${baseUrl}blob/master/EULA.md`,
-  contact: `${baseUrl}blob/master/CONTACT.md`,
+  terms: `http://rialastudio.com/bute/terms/`,
+  licenses: `http://rialastudio.com/bute/copyright/`,
+  eula: `http://rialastudio.com/bute/eula/`,
+  contact: `${baseUrl}issues`,
   standardShare: baseUrl,
   nic: 'http://www.twelvezerothree.com/',
   evan: 'http://www.baconbrix.com/',
 };
+// const baseUrl = 'https://github.com/evanbacon/expo-chat/';
+// export default {
+//   privacy: `${baseUrl}blob/master/PRIVACY.md`,
+//   jobs: `${baseUrl}blob/master/JOBS.md`,
+//   support: `${baseUrl}issues`,
+//   terms: `${baseUrl}blob/master/TERMS.md`,
+//   licenses: `${baseUrl}blob/master/LICENSE.md`,
+//   eula: `${baseUrl}blob/master/EULA.md`,
+//   contact: `${baseUrl}blob/master/CONTACT.md`,
+//   standardShare: baseUrl,
+//   nic: 'http://www.twelvezerothree.com/',
+//   evan: 'http://www.baconbrix.com/',
+// };
